20b331baa583ccc61f3c7b8805b1bfd8aabf9fe3
Curtis Taylor Peterson <ctpeter4@pop-os.localdomain>
2024-02-05 11:14:53 -0700

    Added dependencies to SwissFit

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup, find_packages
+import os as os
 
 VERSION = '0.0.0'
 DESCRIPTION = 'SwissFit'
 LONG_DESCRIPTION = 'SwissFit: A cheesy multitool for complicated least squares fitting'
 
+# Dependencies (https://stackoverflow.com/questions/26900328/install-dependencies-from-setup-py)
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = f"{lib_folder}/requirements.txt"
+install_requires = [] 
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
+
+# SwissFit setup
 setup(
     name = 'swissfit',
-    package_dir = {'':'src/'},
+    package_dir = {'': 'src/'},
     packages = [
         'swissfit',
         'swissfit/numerical_tools',
@@ -24,5 +34,7 @@ setup(
     long_description = LONG_DESCRIPTION,
 
     keywords = ['least squares'],
-    classifiers = ['Programming Language :: Python :: 3']
+    classifiers = ['Programming Language :: Python :: 3'],
+
+    install_requires=install_requires
 )
