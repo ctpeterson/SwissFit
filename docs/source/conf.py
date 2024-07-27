f2195ebdd54bd21e0abdaab1c22beaf547b04465
Curtis Taylor Peterson <ctpeter4@pop-os.localdomain>
2024-07-27 11:11:21 -0600

    Added docs folder to generate Read the Docs

---
+++ docs/source/conf.py
@@ -0,0 +1,45 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# I got a lot of help from: https://pennyhow.github.io/blog/making-readthedocs/
+
+import sys
+import os
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'SwissFit'
+copyright = '2024, Curtis Taylor Peterson'
+author = 'Curtis Taylor Peterson'
+release = '0.2'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon'
+]
+
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_use_ivar = True
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'Python'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'groundwork'
+html_static_path = ['_static']
+
+# -- Paths for autodoc generation --------------------------------------------
+sys.path.insert(0,os.path.abspath('../../src/swissfit/'))
+sys.path.insert(0,os.path.abspath('.'))
