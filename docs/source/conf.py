6876978b21bee39cb664dd63725083058d0bf575
Curtis Taylor Peterson <ctpeter4@pop-os.localdomain>
2024-07-27 11:28:55 -0600

    Change to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,6 +7,7 @@
 
 import sys
 import os
+import sphinx_pdj_theme
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -37,8 +38,8 @@ language = 'Python'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'groundwork'
-html_static_path = ['_static']
+html_theme = 'sphinx_pdj_theme'
+html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
 # -- Paths for autodoc generation --------------------------------------------
 sys.path.insert(0,os.path.abspath('../../src/swissfit/'))
